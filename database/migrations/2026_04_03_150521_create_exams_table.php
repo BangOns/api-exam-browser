a2be7148bcc16dfab3a0e09b9e0140aba455b21d
bangOns <syahr9950@gmail.com>
2026-04-05 12:19:34 +0700

    feat: create exams table migration with schema definition

--- database/migrations/2026_04_03_150521_create_exams_table.php
+++ database/migrations/2026_04_03_150521_create_exams_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('exams', function (Blueprint $table) {
             $table->uuid('id')->primary();
+
             $table->string('name');
             $table->string('subject', 100);
             $table->string('target_class', 50)->nullable();
