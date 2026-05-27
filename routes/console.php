183b8d71d326c93b7f337d0a04aa44a13cefffa6
bangOns <syahr9950@gmail.com>
2026-05-27 10:57:58 +0700

    fixing

--- routes/console.php
+++ routes/console.php
@@ -4,8 +4,8 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
+Artisan::command("inspire", function () {
     $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+})->purpose("Display an inspiring quote");
 
-Schedule::command('exams:update-status')->everyMinute();
+Schedule::command("exams:update-status")->everyMinute();
